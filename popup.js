6c9aa479c6aa4e1b80201244e57d466660ee2c6c
Jitendra Nair <nair.jitendra@gmail.com>
2013-10-01 12:26:14 +0530

    Now you can close tab by clicking on the X mark in the tab entry

--- popup.js
+++ popup.js
@@ -17,6 +17,42 @@ console.log("jtabs loading popup.js");
             }
         });
     };
+    var makeFavicon = function(tab){
+        var img = document.createElement("img");
+        if (tab.favIconUrl &&
+            ( tab.favIconUrl.indexOf("http://" == 0) ||
+              tab.favIconUrl.indexOf("https://") == 0 ) ){
+                  img.src = tab.favIconUrl ;
+                  console.log("favIconUrl:" + tab.favIconUrl);
+              };
+        return img;
+    };
+    var setTabTitle = function(tab){
+        var div_tab_title = document.createElement("div");
+        div_tab_title.className = "tab-title";
+        div_tab_title.innerHTML = tab.title;
+        return div_tab_title;
+    };
+
+    var setTabRemover = function(tabmap){
+        var span = document.createElement("span");
+        span.innerHTML = "X";
+        span.className = "tab-remove";
+        $(tabmap.div_tab).append(span);
+        $(tabmap.div_tab).click(function(){
+            chrome.tabs.remove( $(this).data("tid"));
+            console.log("remove tab");
+        });
+        return tabmap.div_tab;
+    };
+
+    var setActiveTab = function(tabmap){
+        if (tabmap.tab.active) {
+            $(tabmap.div_tab).addClass('active');
+            $(tabmap.div_tab_title).addClass('active');
+        }
+        return tabmap.div_tab;
+    };
 
     var gen_tab_entry = function(tab) {
         var div_tab = document.createElement("div");
@@ -26,24 +62,14 @@ console.log("jtabs loading popup.js");
             chrome.tabs.update( $(this).data("tid"), {active : true} );
             console.log("select tab");
         });
-        var img = document.createElement("img");
-        if (tab.favIconUrl &&
-            ( tab.favIconUrl.indexOf("http://" == 0) ||
-              tab.favIconUrl.indexOf("https://") == 0 ) ){
-                  img.src = tab.favIconUrl ;
-                  console.log("favIconUrl:" + tab.favIconUrl);
-              };
-        var div_tab_title = document.createElement("div");
-        div_tab_title.className = "tab-title";
-        div_tab_title.innerHTML = tab.title;
-        
+        var img = makeFavicon(tab);
         $(div_tab).append(img);
+        var div_tab_title = setTabTitle(tab);
         $(div_tab).append(div_tab_title);
-
-        if (tab.active) {
-            $(div_tab).addClass('active'); 
-            $(div_tab_title).addClass('active'); 
-        }
+        var tabmap =  {tab:tab,
+                       div_tab:div_tab,div_tab_title:div_tab_title};
+        div_tab = setActiveTab(tabmap);
+        div_tab = setTabRemover(tabmap);
         return(div_tab);
     };
 }(window.jtabs = window.jtabs || {},jQuery));
