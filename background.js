1810f181b28fac1e4bfdf5281f685b622ac1d903
Jitendra Nair <nair.jitendra@gmail.com>
2014-02-06 16:54:06 +0530

    Remove BadgeText.

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
-chrome.browserAction.setBadgeText({text: "TL"});
-
+//chrome.browserAction.setBadgeText({text: "TL"});
+/*
 chrome.commands.onCommand.addListener(function(command) {
   console.log('onCommand event received for message: ', command);
-});
+});*/
 
